b2c1fb0be615bfb6d5b57a0f9cc06f5615dd4320
Zhang Maozhen <zhangmaozhen.cn@gmail.com>
2024-10-09 12:43:50 +0800

    modify sh to run f3ba under resnet18

---
+++ sh/flinvert_resnet_20.sh
@@ -0,0 +1,40 @@
+#
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense fedavg --attack flinvert --inject_params --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense deepsight --attack flinvert --inject_params --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense foolsgold --attack flinvert --inject_params --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense mkrum --attack flinvert --inject_params --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense rlr --attack flinvert --inject_params --dataset cifar10 --wandb --task cifar_resnet_20
+#
+#
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense fedavg --attack iba --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense deepsight --attack iba --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense foolsgold --attack iba --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense mkrum --attack iba --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense rlr --attack iba --dataset cifar10 --wandb --task cifar_resnet_20
+
+
+python main.py --config conf_20 --model resnet18 --n_client 20 --defense fedavg --attack f3ba --dataset cifar10 --wandb --task cifar_resnet_20
+python main.py --config conf_20 --model resnet18 --n_client 20 --defense deepsight --attack f3ba --dataset cifar10 --wandb --task cifar_resnet_20
+python main.py --config conf_20 --model resnet18 --n_client 20 --defense foolsgold --attack f3ba --dataset cifar10 --wandb --task cifar_resnet_20
+python main.py --config conf_20 --model resnet18 --n_client 20 --defense mkrum --attack f3ba --dataset cifar10 --wandb --task cifar_resnet_20
+python main.py --config conf_20 --model resnet18 --n_client 20 --defense rlr --attack f3ba --dataset cifar10 --wandb --task cifar_resnet_20
+
+## resnet18 20 client
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense fedavg --attack dba --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense deepsight --attack dba --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense foolsgold --attack dba --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense mkrum --attack dba --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense rlr --attack dba --dataset cifar10 --wandb --task cifar_resnet_20
+#
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense fedavg --attack cerp --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense deepsight --attack cerp --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense foolsgold --attack cerp --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense mkrum --attack cerp --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense rlr --attack cerp --dataset cifar10 --wandb --task cifar_resnet_20
+#
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense fedavg --attack neurotoxin --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense deepsight --attack neurotoxin --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense foolsgold --attack neurotoxin --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense mkrum --attack neurotoxin --dataset cifar10 --wandb --task cifar_resnet_20
+#python main.py --config conf_20 --model resnet18 --n_client 20 --defense rlr --attack neurotoxin --dataset cifar10 --wandb --task cifar_resnet_20
+##
